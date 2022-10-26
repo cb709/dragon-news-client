a602bf617d2be87732d30c4142ecaffb70b4b0dc
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-10-26 23:09:57 +0600

    added fetch in categories

--- src/Pages/Categories/Categories.js
+++ src/Pages/Categories/Categories.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Categories = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },[])
+
     return (
         <div>
             <h3>This is category</h3>
--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categories from '../../Categories/Categories';
 
 const LeftSideNav = () => {
     return (
         <div>
             <h3>Left Side Nav</h3>
+            <Categories></Categories>
         </div>
     );
 };
