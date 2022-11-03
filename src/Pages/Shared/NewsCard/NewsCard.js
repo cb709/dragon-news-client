5d3cd1a53c51cde18b9c4e7ab588bd628aa96134
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-11-03 20:28:21 +0600

    added author details

--- src/Pages/Shared/NewsCard/NewsCard.js
+++ src/Pages/Shared/NewsCard/NewsCard.js
@@ -1,21 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import { FaEye, FaStar, FaBookmark,FaShareAlt } from "react-icons/fa";
+import { Image } from "react-bootstrap";
 
 const NewsCard = ({ news }) => {
   const {
     _id,
     rating,
-    thumbnail_url,
     total_view,
     author,
     details,
     image_url,
     title,
   } = news;
+  console.log(author)
   return (
     <Card className="mb-4">
-      <Card.Header>{author.name ? author.name : 'Name Not Found'}</Card.Header>
+      <Card.Header className="d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <div className="me-3"><Image style={{height:"50px"}} roundedCircle src={author.img}></Image></div>
+          <div>
+            <p className="mb-0">{author.name || "Name Not Found" }</p>
+            <p className="mb-0">{author?.published_date}</p>
+          </div>
+        </div>
+        <div className="d-flex">
+          <FaBookmark className="me-2"></FaBookmark>
+          <FaShareAlt></FaShareAlt>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
@@ -30,7 +44,10 @@ const NewsCard = ({ news }) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="">{total_view}</Card.Footer>
+      <Card.Footer className="d-flex justify-content-between">
+        <div><FaEye className="me-1"></FaEye> {total_view}</div>
+        <div><FaStar></FaStar> {rating.number} {" "} {rating.badge} </div>
+      </Card.Footer>
     </Card>
   );
 };
